5ecd29a713a46507a32d3f32838a88fb1cdd966e
DenDev82 <dendev82@gmail.com>
2024-09-21 11:18:03 +0200

    3/6 OSSC.jsx gotovo

--- src/pages/OSSC.jsx
+++ src/pages/OSSC.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import "../OSSC.css";
 function OSSC() {
-  return <div>OSSC</div>;
+  return (
+    <div className="OSSC">
+      <p>
+        JU Srednjoškolski Centar „Jovan Cvijić“ Modriča nastavlja tradiciju
+        modričkih srednjih škola od davne 1946. godine kada je počela sa radom
+        Škola učenika u privredi, i 1962. godine, od kada u Modriči postoji
+        Gimnazija. U dosadašnjem razvoju, modričke srednje škole su mijenjale
+        organizacione oblike. U prvoj fazi razvoja, odvojeno su djelovale
+        Gimnazija i Škola za učenike u privredi. Zajedničkom odlukom i uz
+        saglasnost prosvjetnih vlasti, ove dvije škole su se spojile u Centar za
+        srednje obrazovanje i vaspitanje, koji počinje sa radom 1. septembra
+        1972. godine. To bi se moglo označiti kao druga faza u razvoju srednjih
+        škola. Treća faza u razvoju srednjih škola u Modriči otpočinje stupanjem
+        na snagu novog Zakona o srednjem obrazovanju po kome se iz sastava
+        Centra za usmjereno obrazovanje i vaspitanje sa dvije radne jedinice
+        izdvajaju i konstituišu dvije škole i to: Gimnazija i
+        Tehničko-tehnološka škola. U Gimnaziju su svrstana zatečena odjeljenja
+        prosvjetne, prevodilačke i matematičke struke i pet novoupisanih
+        odjeljenja (školska 1991/1992. godina). U Tehničko-tehnološkoj školi
+        zadržane su mašinska i hemijska struka, a kasnije je uvedena i
+        elektrotehnička struka. Od školske 2002/2003 godine, odlukom
+        Ministarstva za prosvjetu Republike Srpske, Gimnazija i
+        Tehničko-tehnološka škola spojene su u Srednjoškolski centar „Jovan
+        Cvijić“ Modriča i u tom organizacionom obliku trenutno postoji i radi.
+      </p>
+      <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+      <img src="https://placehold.co/800x600" alt="" />
+    </div>
+  );
 }
 
 export default OSSC;
