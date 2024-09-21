5d3d88674bbfea665bdabce0b6b07175c18d09e9
DenDev82 <dendev82@gmail.com>
2024-09-21 11:12:18 +0200

    2/6 OProjektu.jsx gotovo

--- src/pages/OProjektu.jsx
+++ src/pages/OProjektu.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "../OProjektu.css";
 
 function OProjektu() {
-  return <div>OProjektu</div>;
+  return (
+    <div className="oprojektu">
+      <p>
+        Na infopultu su radili učenici III-4 smjer tehničari informacionih
+        tehnologija na čelu sa profesorom Draganom Todorovićem. <br />
+        <br />
+        Na ideju o izradi infopulta smo došli sasvim spontano. Vodeći neobavezni
+        razgovor na računarskoj sekciji osmislili smo projekat po kome će naša
+        škola JU SŠC „Jovan Cvijić“ u Modriči biti poznata. Na početku smo bili
+        nesigurni, nismo znali da li će nam škola odobriti novčana sredstva za
+        fizičke dijelove infopulta. Profesor Dragan nas je svakim danom
+        usmjeravao i davao nam vjetar u leđa, samim tim kompletan dizajn
+        aplikacije i izvještaj o istoj su završeni u roku od 5 radnih dana.
+        Otišli smo na sastanak kod direktorice i prezentovali naš projekat.
+        Direktorica nam je dala zeleno svjetlo i tada smo otpočeli sa radom.
+        <br />
+        <br />
+        Na dizajnu aplikacije su radili Igor Ilić i Olivera Ristić. U VUE.JS
+        tehnologiji su radili Siniša Savić, Dražen Božić i Borislav Vučićević.
+        Za HTML su bili zaduženi Nikola Pejić, Tijana Mijatović, Dražen Božić i
+        Olivera Ristić. U CSS-u su radili Milan Topalović, Borislav Vučićević i
+        Željana Filipović. Za JavaScript su bili zaduženi Milan Topalović i
+        Borislav Vučićević. Konsultanti su Luka Vujić i Dajana Lučić. Za Python
+        skriptu i OS bio je zadužen Denis Softić.
+        <br />
+        <br />
+        Svaki naš čas, bilo da se održao uživo na računarskoj sekciji ili onlajn
+        preko aplikacije Diskord, bio je prožet pozitivom, osmijesima ali i
+        vrijednim radom. Baš ta kombinacija je doprinjela uspješno završenoj
+        aplikaciji za projekat infopult.
+        <br />
+        <br />
+        Infopult v2.0 uradio Denis Softic učenik IV-2. Cijela aplikacija
+        razvijena u ReactJS :)
+      </p>
+      <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+      <img src="https://placehold.co/800x600" alt="" />
+    </div>
+  );
 }
 
 export default OProjektu;
